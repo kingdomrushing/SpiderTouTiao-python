cff7cbab634d8028881c6bf3eac2b92a72db28ae
kingdomrushing <2422035338@qq.com>
2018-08-15 17:11:46 +0800

    Update pipelines.py

--- TouTiao/TouTiao/pipelines.py
+++ TouTiao/TouTiao/pipelines.py
@@ -14,19 +14,21 @@ class ToutiaoPipeline(object):
         self.helper.open()
         self.tableName = "WorldCup"
         self.platform="今日头条"
-        sql = "create table if not exists %s(" \
-              "datetime varchar(20), " \
-              "platform varchar(10), " \
-              "announcer varchar(15), " \
-              "theme text, " \
-              "content text, " \
-              "url varchar(200), " \
-              "emotion varchar(5), " \
-              "attitude_count varchar(5)," \
-              "repost_count varchar(5), " \
-              "comments_count varchar(5), " \
-              "attention varchar(5), " \
-              "sensitiveWords varchar(100)) "  % (self.tableName)
+        sql="""    create table if not exists %s(
+	               id int auto_increment primary key,
+	               datetime varchar(20),
+	               platform varchar(10),
+	               announcer varchar(15),
+	               theme text,
+	               content text,
+	               url text,
+	               emotion varchar(5),
+	               attitude_count varchar(20),
+	               repost_count varchar(20),
+	               comments_count varchar(20),
+	               attention varchar(20),
+	               sensitiveWords varchar(100) )
+	               COLLATE='utf8_general_ci';""" % (self.tableName)
         self.helper.getCursor().execute(sql)
 
     def process_item(self, item, spider):
